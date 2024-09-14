51297e19886660052e29ad67db64d23ceb006e84
zmdsn <zmdsn@126.com>
2024-09-14 18:57:41 +0800

    Update raindrop.py

--- rainpy/raindrop.py
+++ rainpy/raindrop.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
- 
+
 import argparse
 import os
 import datetime
 
+py_head = """#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
 
+sh_head = """#!/bin/sh 
+"""
 
 BSD_3 = """
 Copyright {year} {author}
@@ -20,8 +25,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-set_up = """
-# -*- coding: utf-8 -*-
+set_up = py_head + """
 # Learn more: https://github.com/kennethreitz/setup.py
 
 from setuptools import setup, find_packages
@@ -35,7 +39,7 @@ with open('LICENSE') as f:
 setup(
     name='{name}',
     version='0.1.0',
-    description='Sample package',
+    description='description for Sample package',
     long_description=readme,
     author='{author}',
     author_email='{email}',
@@ -53,9 +57,7 @@ This simple project is an example of a rainpy project build.
 [Learn more](https://github.com/zmdsn/rainpy)
 """
 
-tests_init = """
-# -*- coding: utf-8 -*-
-
+tests_init = py_head + """
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -63,25 +65,26 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import {name}
 """
 
-start_sh = """
+start_sh = sh_head + """
 cd {folder}
 
 pip install ipython
 pip install pipenv
 pip install pytest
+pip install ipykernel
 
 echo python --version
 echo "install all success"
 
-git config user.name {name}
-git config user.email {email}
+git config --global user.name {name}
+git config --global user.email {email}
 rm -rf .git
 git init
 git add .
 git commit -m "generate by rainpy"
 """
 
-test_sh = """
+test_sh = sh_head + """
 mkdir .venv
 
 pip install pipenv
@@ -95,7 +98,7 @@ echo "install all success"
 
 # 执行 并 统计覆盖率
 pipenv run pytest --cov=. --cov-report=html:reports/coverage_report --cov-config=.coveragerc --html=reports/testresult.html --self-contained-html
-""" 
+"""
 
 make_require = "pipreqs ./ --encoding UTF8 --force --use-local --ignore .venv"
 
@@ -103,67 +106,93 @@ gitignore_list = """
 .coverage
 reports/
 *.pyc
+.venv
+.vscode
+.pytest_cache
 """
+
+test_example = py_head + """
+import pytest
+import {name}
+
+def test_passing():
+    assert (1, 2, 3) == (1, 2, 3)
+"""
+
 
 def make_foleder(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
+
 def make_file(path, content, mode="w"):
-    with open(path,mode) as f:
+    with open(path, mode) as f:
         f.write(content)
 
 
-licenes = {"BSD":BSD_3}
+licenes = {"BSD": BSD_3}
+
 
 def get_year():
     now = datetime.datetime.now()
     return now.year
+
 
 def make_license(args):
     license = BSD_3.format(year=get_year(), author=args.author)
     file_path = os.path.join(args.floder, "LICENSE")
     make_file(file_path, license)
 
+
 def make_setup(args):
     set_up_content = set_up.format(name=args.name,
-                            author=args.author,
-                            email=args.email,
-                            url=args.url)
+                                   author=args.author,
+                                   email=args.email,
+                                   url=args.url)
     file_path = os.path.join(args.floder, "set_up.py")
     make_file(file_path, set_up_content)
+
 
 def make_read_me(args):
     read_me_content = read_me.format(name=args.name)
     file_path = os.path.join(args.floder, "README.md")
     make_file(file_path, read_me_content)
 
+
 def make_tests_init(args):
     tests_init_content = tests_init.format(name=args.name)
     file_path = os.path.join(args.floder, "tests", "__init__.py")
     make_file(file_path, tests_init_content)
 
+
 def make_start(args):
     start_sh_content = start_sh.format(folder=args.floder,
                                        name=args.name,
-                                 email=args.email)
+                                       email=args.email)
     file_path = os.path.join(args.floder, "start.sh")
     make_file(file_path, start_sh_content)
     os.system(f"sh {file_path}")
 
 
-def make_req():
+def make_require_sh(args):
     file_path = os.path.join(args.floder, "make_require.sh")
     make_file(file_path, make_require)
 
 
-def make_req(args):
+def make_test_sh(args):
     file_path = os.path.join(args.floder, "test.sh")
     make_file(file_path, test_sh)
 
+
 def make_ignore(args):
     file_path = os.path.join(args.floder, ".gitignore")
-    make_file(file_path, test_sh)
+    make_file(file_path, gitignore_list)
+
+
+def make_test_exm(args):
+    test_example_content = test_example.format(name=args.name)
+    file_path = os.path.join(args.floder, "tests", f"test_{args.name}.py")
+    make_file(file_path, test_example_content)
 
 
 def make_frame(args):
@@ -172,33 +201,42 @@ def make_frame(args):
     for sub_folder in [".venv", "doc", "tests", args.name]:
         sub_path = os.path.join(folder, sub_folder)
         make_foleder(sub_path)
-        
+
     make_license(args)
     make_setup(args)
     make_read_me(args)
     make_tests_init(args)
-    make_req(args)
+    make_require_sh(args)
+    make_test_sh(args)
     make_ignore(args)
-    
-    for file in [".gitignore", "requirements.txt", "lab.ipynb"]:
+    make_test_exm(args)
+
+    for file in ["requirements.txt", "lab.ipynb"]:
         file_path = os.path.join(folder, file)
         make_file(file_path, "")
 
     for file in ["__init__.py", "core.py", "utils.py"]:
         file_path = os.path.join(folder, args.name, file)
         make_file(file_path, "")
+
     make_start(args)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='argparse testing')
-    parser.add_argument('--name','-n',type=str, default = "raindrop", required=True,help="Your python package name")
-    parser.add_argument('--floder','-f',type=str, default="./raindrop", required=True, help='The folder you want to put in')
-    parser.add_argument('--author','-a',type=str, default="zmdsn" , help='your name')
-    parser.add_argument('--email','-e',type=str, default="zmdsn@126.com",  help='your email')
-    parser.add_argument('--url','-u',type=str, default="", help='the url in git')
+    parser.add_argument('--name', '-n', type=str, default="raindrop",
+                        required=True, help="Your python package name")
+    parser.add_argument('--floder', '-f', type=str, default="./raindrop",
+                        required=True, help='The folder you want to put in')
+    parser.add_argument('--author', '-a', type=str,
+                        required=True, default="zmdsn", help='your name')
+    parser.add_argument('--email', '-e', type=str,
+                        required=True, default="zmdsn@126.com",  help='your email')
+    parser.add_argument('--url', '-u', type=str,
+                        default="", help='the url in git')
     args = parser.parse_args()
     make_frame(args)
-    
-    
+
+
 # export PATH=$PATH:xx
 # raindrop.py --name ss --floder  -a zmdsn -e zmdsn@126.com
