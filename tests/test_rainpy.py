13cffc1563475817bbb906dd4663bb252c2bd0e8
zmdsn <zmdsn@126.com>
2024-09-19 09:42:21 +0800

    test_pd_read_csv

--- tests/test_rainpy.py
+++ tests/test_rainpy.py
@@ -11,3 +11,15 @@ def test_raindrop():
                 "url":"https://github.com/zmdsn/rainpy"}
     RainDrop = namedtuple("RainDrop", drops.keys())
     drop = RainDrop(**drops)
+
+
+
+def test_pd_read_csv():
+    df_gbk_t = pd_read_csv("tests/test_data/csv_gbk_t.csv",  sep="\t")
+    df_gbk = pd_read_csv("tests/test_data/csv_gbk.csv")
+    df_utf8 = pd_read_csv("tests/test_data/csv_utf8.csv")
+    df_utf8_t = pd_read_csv("tests/test_data/csv_utf8_t.csv", sep="\t")
+    
+    all(df_utf8.fillna(0) == df_gbk.fillna(0))
+    all(df_utf8_t.fillna(0) == df_utf8.fillna(0))
+    all(df_utf8_t.fillna(0) == df_gbk_t.fillna(0))
