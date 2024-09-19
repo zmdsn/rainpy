bb8acec615ad2a964076b6764afe1880bdd497b6
zmdsn <zmdsn@126.com>
2024-09-19 09:58:13 +0800

    read_csv

--- rainpy/utils.py
+++ rainpy/utils.py
@@ -1,6 +1,24 @@
 
 import pandas as pd
 
+def read_csv(filename: str, return_type: str = 'dict'):
+    # type : 'list', 'dict'
+    encodings = ['utf-8', 'gbk',  'utf-8-sig', 'GB2312', 'gb18030',]
+    data = []
+    for encoding in encodings:
+        try:
+            with open(filename, encoding=encoding) as f:
+                if return_type == 'dict':
+                    reader = csv.DictReader(f)
+                else:
+                    reader = csv.reader(f)
+                header = next(reader)
+                for row in reader:
+                    data.append(row)
+            return data
+        except Exception as e:
+            print(filename, e)
+    return data
 
 def pd_read_csv(filename, **kwargs):
     encodings = ['utf-8', 'gbk',  'utf-8-sig', 'GB2312', 'gb18030',]
