a27279cb05e1dcad062044987a2f8f57c266a87c
Rui Li <zmdsn@126.com>
2024-09-26 15:01:47 +0800

    Update utils.py

--- rainpy/utils.py
+++ rainpy/utils.py
@@ -39,3 +39,35 @@ def pd_read_csv(filename, **kwargs):
 def get_extension(file_path):
     return os.path.splitext(file_path)[-1]
 
+
+def formatTime(atime):
+    import time
+    return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(atime))
+
+fileinfo = os.stat(file_path)
+print("最后一次访问时间:",formatTime(fileinfo.st_atime))
+print("最后一次修改时间:",formatTime(fileinfo.st_mtime))
+print("最后一次状态变化的时间：",formatTime(fileinfo.st_ctime))
+print("索引号：",fileinfo.st_ino)
+print("被连接数目：",fileinfo.st_dev)
+print("文件大小:",fileinfo.st_size,"字节")
+print("最后一次访问时间:",fileinfo.st_atime)
+print("最后一次修改时间:",fileinfo.st_mtime)
+print("最后一次状态变化的时间：",fileinfo.st_ctime)
+
+
+import psutil
+psutil.cpu_count() 
+psutil.cpu_count(logical=False) 
+
+psutil.virtual_memory()
+psutil.disk_partitions()
+psutil.disk_usage('/dev/sda1')
+
+import sys
+
+sys.getsizeof(dfe)
+
+
+
+
